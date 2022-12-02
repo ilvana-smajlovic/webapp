0efc95c679118d8358d03df2d75c098781647833
Hamza Hebibovic <hamza.hebibovic@edu.fit.ba>
2022-12-02 12:46:40 +0100

    Added media page

---
+++ TracksterAngular/src/app/movie-watchlist/movie-watchlist.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MovieWatchlistComponent } from './movie-watchlist.component';
+
+describe('MovieWatchlistComponent', () => {
+  let component: MovieWatchlistComponent;
+  let fixture: ComponentFixture<MovieWatchlistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MovieWatchlistComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MovieWatchlistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ TracksterAngular/src/app/movie-watchlist/movie-watchlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-watchlist',
+  templateUrl: './movie-watchlist.component.html',
+  styleUrls: ['./movie-watchlist.component.css']
+})
+export class MovieWatchlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
