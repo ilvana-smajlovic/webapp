c805829e024942873056ae0bdad5e594b7d7d8fc
ilvana.smajlovic <ilvana.smajlovic@edu.fit.ba>
2023-03-28 20:32:30 +0200

    Logiranje

---
+++ Trackster.API/Logger/dbLoggerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Trackster.API.Logger
+{
+    public class dbLoggerOptions
+    {
+        public string ConnectionString { get; init; }
+        public string[] LogFields { get; init; }
+        public string LogTable { get; init; }
+
+        public dbLoggerOptions()
+        {
+        }
+    }
+}
---
+++ Trackster.Core/CustomError.cs
@@ -0,0 +1,11 @@
+﻿namespace Trackster.Core
+{
+    public class CustomError
+    {
+        public int Id { get; set; }
+        public string Values { get; set; }
+        public DateTime Created { get; set; }
+
+        public CustomError() { }
+    }
+}
